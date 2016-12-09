b7f0be1f034a4e957dadfc154c7bad7e04b3f864
Alexandra Vovk <avovkdesign@gmail.com>
2016-12-09 15:33:41 +0200

    1.0

--- functionsphp.php
+++ functionsphp.php
@@ -1,15 +1,98 @@
 <?php
 /* ====================================
  * Plugin Name: Functions.PHP
- * Description: Мой новый functions.php. Отредактируйте плагин, чтобы добавить в него код, который обычно требуется вставить в файл function.php активной темы. 
- * Plugin URI: http://avovkdesign.com/function-php-chem-zamenit.html
- * Author: Alexandra Vovk
- * Author URI: http://avovkdesign.com/
+ * Description: My new functions.php. Edit plugin to add your custom code without editing function.php in theme
+ * Plugin URI: http://wp-puzzle.com/functionsphp/
+ * Author: WP Puzzle
+ * Author URI: http://wp-puzzle.com/
  * Version: 1.0
  * ==================================== */
 
+/* =================================================================================
+ * 
+ * ================================================================================= */
 function functionsphp_head_info(){
-	echo "\n<!-- Плагин Functions.PHP активен -->\n";
+
+	echo "\n<!-- Plugin Functions.PHP active -->\n";
+
 }
 add_action('wp_head', 'functionsphp_head_info');
 
+
+
+/* =================================================================================
+ * your code in <head>
+ * ================================================================================= */
+function functionsphp_head(){
+
+    $before = <<<EOT
+<!-- your code in <head> -->
+EOT;
+
+}
+add_filter( 'wp_head', 'functionsphp_head' );
+/* ================================================================================= */
+
+
+
+
+
+/* =================================================================================
+ * your code before </body>
+ * ================================================================================= */
+function functionsphp_in_footer( $content ){
+
+    echo <<<EOT
+<!-- your code before </body> -->
+EOT;
+
+}
+add_action( 'wp_footer', 'functionsphp_in_footer' );
+/* ================================================================================= */
+
+
+
+
+
+/* =================================================================================
+ * your code before and after post entry
+ * ================================================================================= */
+function functionsphp_html_in_content( $content ) {
+ 
+    if ( !is_single() ) {
+        return $content;
+    }
+
+    $before = <<<EOT
+<!-- your code before post entry -->
+EOT;
+
+    $after = <<<EOT
+<!-- your code after post entry -->
+EOT;
+
+   return $before . $content . $after;
+
+}
+add_filter( 'the_content', 'functionsphp_html_in_content' );
+/* ================================================================================= */
+
+
+
+
+/* =================================================================================
+ * styles and scripts inlude
+ *
+ * 1. upload your files into plugin folder /wp-content/plugins/functionsphp/ through FTP or file manager in hosting panel
+ * 2. use following code for connecting uploaded files, examples, mystyle.css and myscript.js
+ * 3. uncomment string add_action( 'wp_enqueue_scripts', 'functionsphp_style_and_script', 100 );
+ *
+ * ================================================================================= */
+function functionsphp_style_and_script() {
+ 
+    wp_enqueue_style(  'mystyle',  plugin_dir_url( __FILE__ ) .'mystyle.css' , array(), true);
+    wp_enqueue_script( 'myscript', plugin_dir_url( __FILE__ ) .'myscript.js' , array(), true, true );
+
+}
+//add_action( 'wp_enqueue_scripts', 'functionsphp_style_and_script', 100 );
+/* ================================================================================= */
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== FunctionsPHP ===
+Contributors: wppuzzle, avovkdesign
+Donate link: http://avovkdesign.com/bymecoffee
+Tags: functions, code
+Requires at least: 3.0
+Tested up to: 4.5
+Stable tag: 1.0
+
+Safe file replacement functions.php
+
+== Description ==
+
+To add HTML code counters and scripts — use examples in plugin code. Simply inserted HTML code in plugin will be displayed incorrectly and can lead to site malfunction.
+
+
+
+== Installation ==
+
+= Automatic installation from archive =
+
+1. In console WordPress go to menu Plugins -> Add new.
+1. At top of page, next to inscription Add plugins, click Upload new.
+1. In appeared window click Choose file and select saved archive with plugin on computer. Then click button Install.
+1. After plugin installation, system offers to activate it — click link Activate plugin
+
+
+
+== Changelog ==
+
+= 1.0 =
+* Init release
